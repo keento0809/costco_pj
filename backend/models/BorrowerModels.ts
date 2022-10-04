eb49f5fd99ea368536e641c656749677e9b57bd4
KEENTO <handball0321@gmail.com>
2022-10-04 15:19:08 -0700

    Added Schema to each Model files by using Mongoose

--- backend/models/BorrowerModels.ts
+++ backend/models/BorrowerModels.ts
@@ -1,4 +1,5 @@
 import { History } from "./HistoryModels";
+import mongoose from "mongoose";
 
 export interface Borrower {
   borrowerId: string;
@@ -7,3 +8,31 @@ export interface Borrower {
   password: string;
   histories: History[];
 }
+
+const borrowerSchema = new mongoose.Schema<Borrower>(
+  {
+    borrowerId: {
+      type: String,
+      require: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    histories: {
+      type: [],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Borrower", borrowerSchema);
--- backend/models/HolderModels.ts
+++ backend/models/HolderModels.ts
@@ -1,4 +1,5 @@
 import { History } from "./HistoryModels";
+import mongoose from "mongoose";
 
 interface SocialMediaLinksObj {
   twitterLink: string;
@@ -31,3 +32,51 @@ export interface Holder {
   reviews: ReviewObj[];
   histories: History[];
 }
+
+const holderSchema = new mongoose.Schema<Holder>(
+  {
+    holderId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    socialMediaLinks: {
+      type: {},
+      required: true,
+    },
+    averageRating: {
+      type: Number,
+      required: true,
+    },
+    users: {
+      type: Number,
+      required: true,
+    },
+    followers: {
+      type: Number,
+      required: true,
+    },
+    nextSchedules: {
+      type: [],
+      required: true,
+    },
+    reviews: {
+      type: [],
+      required: true,
+    },
+    histories: {
+      type: [],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Holder", holderSchema);
