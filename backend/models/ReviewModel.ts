62adda74e320c981e4cf0b28132f549b245d0dd4
Misato Tanno <breitmeer.0804@gmail.com>
2022-10-06 13:55:43 -0700

    Added Schedule/Review schemas

--- backend/models/ReviewModel.ts
+++ backend/models/ReviewModel.ts
@@ -1,18 +1,39 @@
-/**
- * TODO:
- * How connect each review to users
- */
+import mongoose from "mongoose";
 
-interface User {
-    name: string,
-    avatar ? : string
+interface Review {
+    reviewId: string;
+    holderId: string;
+    date: Date;
+    description: string;
+    rating: number;
 }
 
-export interface Review {
-    reviewFrom : User;
-    description : string;
-    rating : number;
-}
+const reviewSchema = new mongoose.Schema<Review>(
+    {
+        reviewId: {
+            type: String,
+            required: true
+        },
+        holderId: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            required: [true, "Please fill in the date you went to Costco"]
+        },
+        description: {
+            type: String,
+            trim: true,
+            minlength: [5, "description must be above 5 characters"]
+        },
+        rating: {
+            type: Number,
+            min: [1, 'Rating must be above 1.0'],
+            max: [5, 'Rating must be below 5.0'],
+            required: true
+        }
+    }
+);
 
-
-
+export  default mongoose.model('Review', reviewSchema)
--- backend/models/ScheduleModel.ts
+++ backend/models/ScheduleModel.ts
@@ -1,14 +1,36 @@
-interface User {
-    name: string,
-    userDescription: string,
-    avatar ? : string,
-    contactDetail: string,
-    price: number
+import mongoose from "mongoose";
+
+interface Schedule {
+    scheduleId: string,
+    borrowerId: string,
+    holderId: string,
+    date: Date,
+    costcoPlace: string
 }
 
-export interface Schedule {
-    date: Date;
-    time: string;
-    place: string;
-    meetupUser: User;
-}
+const scheduleSchema = new mongoose.Schema<Schedule>(
+    {
+        scheduleId: {
+            type: String,
+            required: true,
+        },
+        borrowerId: {
+            type: String,
+            required: true
+        },
+        holderId: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        },
+        costcoPlace: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+export default mongoose.model("Schedule", scheduleSchema)
