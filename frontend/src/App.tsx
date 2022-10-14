4d6b20c7c41cbd46f95c34a30f63b4772258ccb3
Koki Sakai <likuor0528@gmail.com>
2022-10-14 13:28:25 -0700

    Add user-detail

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import UserDetail from './pages/UserDetail';
+
 function App() {
   return (
     <div className='App'>
       <h1>Costco Test by Koki</h1>
+      <UserDetail />
     </div>
   );
 }
