75b89f7ab5a93e40a0886c83a7eb86e223bd972b
Misato Tanno <breitmeer.0804@gmail.com>
2022-10-14 10:04:32 -0700

    m

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,18 @@
+// @ts-ignore
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Home");
 });
+app.use('/auth', )
 
 app.listen(port, () => {
   console.log(`[server] server is running on port ${port}.`);
