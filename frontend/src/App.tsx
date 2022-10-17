cbd46836fdaae45adf565f1628da290ad5277beb
Atsuya Tanaka <tenmusu007@gmail.com>
2022-10-17 15:23:22 -0700

    add theme file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import Theme from './theme/Theme';
+import { ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { colorTheme } from "./theme/Theme";
+
 
 function App() {
   return (
-    <ThemeProvider theme={Theme}>
-      <div className='App'>
-        <Typography variant='h3'>Responsive h3</Typography>
-      </div>
-    </ThemeProvider>
-  );
+		<div className='App'>
+			<ThemeProvider theme={colorTheme}></ThemeProvider>
+		</div>
+	);
 }
 
 export default App;
+
+
