72bc8f0cd4a5f8c82a84325fdd9ee7526e0e1f8b
Koki Sakai <likuor0528@gmail.com>
2022-10-17 11:50:12 -0700

    Add theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import Theme from './theme/Theme';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Costco Test by Koki</h1>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <div className='App'>
+        <Typography variant='h3'>Responsive h3</Typography>
+      </div>
+    </ThemeProvider>
   );
 }
 
