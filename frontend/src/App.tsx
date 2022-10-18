8f0f8a1a2789cfa8f4e86291d1e93d932aa3105a
Atsuya Tanaka <tenmusu007@gmail.com>
2022-10-17 22:24:51 -0700

    fixed app.tsx for not having conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,12 @@ import * as React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { colorTheme } from "./theme/Theme";
-import Profile from "./pages/Profile";
 
 function App() {
 	return (
 		<div className='App'>
       <ThemeProvider theme={colorTheme}>
-        
-			<Profile />
+    
       </ThemeProvider>
 		</div>
 	);
